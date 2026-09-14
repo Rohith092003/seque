87e0ea5923bed01b5952adc69cd867658a53105e
Rohith092003 <rvoddepelli@gmail.com>
2026-09-15 02:10:00 +0530

    Fix: bust CSS cache to force grid line removal on live site (v1.0.2)

--- functions.php
+++ functions.php
@@ -64,7 +64,7 @@ function seque_enqueue_scripts() {
         'seque-style',
         get_stylesheet_uri(),
         ['seque-google-fonts'],
-        '1.0.0'
+        '1.0.2'
     );
 
     // Enqueue main interactive script
